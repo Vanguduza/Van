5615b35d282330fa36ee4b026dfb6ef9f1315fe7
Vanguduza <tapiwaguduza@gmail.com>
2026-09-20 21:14:42 +0200

    C28: add Rev 5.1 owner cognition read model

---
+++ trading/vati/readmodels/__init__.py
@@ -0,0 +1,3 @@
+from vati.readmodels.cognition import MODEL_HIERARCHY, READ_MODEL_VERSION, build_cognition_read_model
+
+__all__ = ["MODEL_HIERARCHY", "READ_MODEL_VERSION", "build_cognition_read_model"]
